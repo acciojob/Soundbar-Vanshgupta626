98f88e6814f35bc3543a163484ee885cedda0e62
Vansh gupta <vanshg6532@gmail.com>
2026-02-14 15:15:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+const stopBtn = document.querySelector(".stop");
+
+let currentAudio = null;
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+
+    if(currentAudio){
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+
+    const soundName = btn.dataset.sound;
+    currentAudio = new Audio(`sounds/${soundName}.mp3`);
+    currentAudio.play();
+  });
+});
+
+stopBtn.addEventListener("click", () => {
+  if(currentAudio){
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+});
